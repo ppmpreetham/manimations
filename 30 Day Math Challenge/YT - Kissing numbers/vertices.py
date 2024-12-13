67c24b85d9f8e3ef702eba9918a61c76f9563f6e
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-12-14 02:02:26 +0530

    update:vertices

--- 30 Day Math Challenge/YT - Kissing numbers/vertices.py
+++ 30 Day Math Challenge/YT - Kissing numbers/vertices.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import combinations
 
 vertices_4d = [
     [1, 1, 0, 0], [1, -1, 0, 0], [-1, 1, 0, 0], [-1, -1, 0, 0],
@@ -15,7 +16,6 @@ vertices_3d = []
 
 for v in vertices_4d:
     x, y, z, w = v
-    
     x_3d = x / (1 - w / D)
     y_3d = y / (1 - w / D)
     z_3d = z / (1 - w / D)
@@ -23,12 +23,30 @@ for v in vertices_4d:
 
 vertices_3d = np.array(vertices_3d)
 
-fig = plt.figure()
+def squared_distance_4d(v1, v2):
+    return sum((a - b) ** 2 for a, b in zip(v1, v2))
+
+edges = []
+for i, j in combinations(range(24), 2):
+    if squared_distance_4d(vertices_4d[i], vertices_4d[j]) == 2:
+        edges.append((i, j))
+
+fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(vertices_3d[:, 0], vertices_3d[:, 1], vertices_3d[:, 2])
+
+ax.scatter(vertices_3d[:, 0], vertices_3d[:, 1], vertices_3d[:, 2], 
+          color='blue', s=50)
+
+for edge in edges:
+    start, end = edge
+    ax.plot([vertices_3d[start, 0], vertices_3d[end, 0]],
+            [vertices_3d[start, 1], vertices_3d[end, 1]],
+            [vertices_3d[start, 2], vertices_3d[end, 2]], 
+            color='red', linewidth=1)
 
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
+ax.view_init(elev=20, azim=45)
 
 plt.show()
